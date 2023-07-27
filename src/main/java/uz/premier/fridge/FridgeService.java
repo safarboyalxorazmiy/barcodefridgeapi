be850cbcc6eda7189c583bd7a9c5104a0f6d1535
Safarboy <safarboyalkhwarizmi@gmail.com>
2023-07-27 09:53:55 +0500

    imports removed

--- src/main/java/uz/premier/fridge/FridgeService.java
+++ src/main/java/uz/premier/fridge/FridgeService.java
@@ -1,6 +1,5 @@
 package uz.premier.fridge;
 
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -12,11 +11,7 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.springframework.stereotype.Service;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.time.LocalDate;
 import java.util.Calendar;
 
